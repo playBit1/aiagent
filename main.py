cb384f79c46d7065e61b644a98359f3c42b4ad52
playBit1 <binulben5@gmail.com>
2026-04-30 11:51:28 +1000

    Basic Agent Built

---
+++ main.py
@@ -0,0 +1,32 @@
+import argparse
+import os
+
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if not api_key:
+    raise Exception("LLM API KEY NOT FOUND")
+
+client = genai.Client(api_key=api_key)
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+
+message = types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+response = client.models.generate_content(model="gemini-2.5-flash", contents=message)
+
+if not response.usage_metadata:
+    raise Exception("Request Failed!")
+
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+print(f"Response:\n{response.text}")
